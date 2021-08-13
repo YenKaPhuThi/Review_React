8c5ffce6bfbad655ffcfb851ef1b60f9b83cf6de
Yen Ka <yenkaphu96pnv@gmail.com>
2021-08-13 16:55:37 +0700

    [React]: Riew state and useState

--- src/components/CauTrucDieuKhien_Rerender/DemoIf.jsx
+++ src/components/CauTrucDieuKhien_Rerender/DemoIf.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 export default class DemoIf extends Component {
-  // Thuộc tính
-  isLogin = true;
-  userName = "";
+  constructor(props) {
+    super(props);
+    // this.state là thuộc tính có sẵn của component, chứa các thuộc tính có khả năng thay đổi bởi 1 sự kiện nào đó của component
+    this.state = {
+      isLogin: false,
+      userName: "",
+    };
+  }
+
+  login = () => {
+    // this.setState(): là phương thức kế thừa từ class componen => thay đổi giá trị state và gọi hàm render => render lại giao diện
+    // setState là phương thức bất đồng bộ
+    this.setState({
+      isLogin: true,
+      userName: "Yen Ka",
+    }, () => {console.log(this.state)});
+  };
+
+  logout = () => {
+    this.setState({
+      isLogin: false,
+      userName: "",
+    }, () => {console.log(this.state)});
+  }
 
   // Cách 1: Dùng phương thức kết hợp if để xác định nội dung render giao diện
   // renderContent = () => {
@@ -26,14 +47,14 @@ export default class DemoIf extends Component {
     return (
       <div>
         {/* {this.renderContent()} */}
-        
-        {this.isLogin ? (
+
+        {this.state.isLogin ? (
           <div>
-            Hello {this.userName} FE <button>Log out</button>
+            Hello {this.state.userName} FE <button onClick={this.logout}>Log out</button>
           </div>
         ) : (
           <div>
-            <button>Login</button>
+            <button onClick={this.login}>Login</button>
           </div>
         )}
       </div>
