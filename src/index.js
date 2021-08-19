b0b9e262576e477f18e242bf962e4d5cdde860e4
Yen Ka <yenkaphu96pnv@gmail.com>
2021-08-19 07:18:13 +0700

    [Reactjs]: Setup redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// setup redux
+import { Provider } from "react-redux";
+import { createStore} from "redux";
+import {rootReducer} from "./redux/reducers/rootReducer";
+
+// Tạo store tổng của ứng dụng
+const store = createStore(rootReducer);
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducers/rootReducer.jsx
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+// store tổng ứng dụng
+export const rootReducer = combineReducers({
+  // Nơi chứa reducer cho nghiệp vụ (store con)
+});
